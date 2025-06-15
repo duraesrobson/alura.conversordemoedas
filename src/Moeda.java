af6db260d857c2926c4985f339e0c8f69ac49387
Robson Durães <duraesrobson@gmail.com>
2025-06-15 14:23:43 -0300

    feat: interacao menu

--- src/Moeda.java
+++ src/Moeda.java
@@ -1,9 +1,11 @@
-public record Moeda(String base_code, String target_code, String conversion_result) {
+
+
+public record Moeda(String base_code, String target_code, String conversion_result, String amount) {
 
     @Override
     public final String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Resultado: ").append(conversion_result);
+        sb.append(conversion_result);
         return sb.toString();
 
     }
--- src/Principal.java
+++ src/Principal.java
@@ -8,25 +8,76 @@ public class Principal {
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Digite a opção: ");
-        var escolha = scanner.nextLine();
-        String moedaBase = "";
-        String moedaAlvo = "";
-        String valor = "";
+        var escolha = "";
 
-        if (escolha.equalsIgnoreCase("1")) {
-            moedaBase = "USD";
-            moedaAlvo = "BRL";
+        while (!escolha.equals("7")) {
+        
+            System.out.println("""
+                            Digite a opção de conversão:
+
+                            1) Real Brasileiro =>> Euro\r 
+                            2) Euro =>> Real Brasileiro\r
+                            3) Real Brasileiro =>> Dólar\r
+                            4) Dólar =>> Real Brasileiro\r
+                            5) Real Brasileiro =>> Peso Mexicano\r
+                            6) Peso Mexicano =>> Real Brasileiro\r
+                            7) Sair
+                            """);
+
+            // if (escolha.equals("7")) {
+            //     break;
+            // }
+                            
+            escolha = scanner.nextLine();
+            System.out.println();
+            String moedaBase = "";
+            String moedaAlvo = "";
+            String valor = "";
+
+            switch (escolha) {
+                case "1":
+                    moedaBase = "BRL";
+                    moedaAlvo = "EUR";
+                    break;
+                case "2":
+                    moedaBase = "EUR";
+                    moedaAlvo = "BRL";
+                    break;
+                case "3":
+                    moedaBase = "BRL";
+                    moedaAlvo = "USD";
+                    break;
+                case "4":
+                    moedaBase = "USD";
+                    moedaAlvo = "BRL";
+                    break;
+                case "5":
+                    moedaBase = "BRL";
+                    moedaAlvo = "MXN";
+                    break;
+                case "6":
+                    moedaBase = "MXN";
+                    moedaAlvo = "BRL";
+                    break;
+                case "7":
+                    continue;
+                default:
+                    System.out.println("Digite uma opção válida!\n");
+                    continue;
+            }
+            
+            System.out.print("Digite o valor que deseja converter: ");
+            valor = scanner.nextLine();
+            System.out.println();
+
+            ConversorMoeda conversor = new ConversorMoeda();
+            Moeda resultado = conversor.ConversorMoeda(moedaBase, moedaAlvo, valor);
+
+            System.out.println("O valor de " + valor + "(" + moedaBase + ")" + " corresponde ao valor final de " + resultado + "(" + moedaAlvo + ")" + "\n");
+
         }
-        
-        System.out.print("Digite o valor que deseja converter: ");
-        valor = scanner.nextLine();
 
-        ConversorMoeda conversor = new ConversorMoeda();
-        Moeda resultado = conversor.ConversorMoeda(moedaBase, moedaAlvo, valor);
-
-        System.out.println(resultado);
+        System.out.println("Programa finalizado!");
     
     }
 }
